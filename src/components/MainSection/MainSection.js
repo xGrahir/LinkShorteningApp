5bed491afcddb8a336d1346f9bd1f603971fbbe2
xGrahir <tomasz.pietron95@gmail.com>
2024-06-06 12:20:32 +0200

    update

--- src/components/MainSection/MainSection.js
+++ src/components/MainSection/MainSection.js
@@ -5,18 +5,17 @@ import { About } from './About'
 
 export const MainSection = () => {
 	return (
-
-			<div className={styles['section-background']}>
-				<div className={styles.wrapper}>
-					<Shorten />
-					<HeadText
-						styles={styles.text}
-						pstyles={styles.paragraph}
-						title={'Advanced Statistics'}
-						about={'Track how your links are performing across the web with our advanced statistics dashboard.'}
-						tag={'h2'}></HeadText>
-					<About />
-				</div>
+		<div className={styles['section-background']}>
+			<div className={styles.wrapper}>
+				<Shorten />
+				<HeadText
+					styles={styles.text}
+					pstyles={styles.paragraph}
+					title={'Advanced Statistics'}
+					about={'Track how your links are performing across the web with our advanced statistics dashboard.'}
+					tag={'h2'}></HeadText>
+				<About />
 			</div>
+		</div>
 	)
 }
